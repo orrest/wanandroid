7645e79d1fc6a63874360c65d79cb0c84a3bcd55
zzhxufeng <zzhxufeng@163.com>
2022-06-06 16:19:42 +0800

    [navigation] basic implementation.

--- app/src/main/java/name/zzhxufeng/wanandroid/ui/theme/Theme.kt
+++ app/src/main/java/name/zzhxufeng/wanandroid/ui/theme/Theme.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import name.zzhxufeng.wanandroid.repository.WanAndroidNetwork
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -28,7 +29,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun WanandroidTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun WanAndroidTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
